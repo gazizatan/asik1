48487087e99f9b782169164cfc07d0d6b1e83b8e
gazizatan <153696424+gazizatan@users.noreply.github.com>
2024-12-21 23:37:27 +0500

    let's do this

---
+++ index.js
@@ -0,0 +1,18 @@
+class Transaction {
+    constructor(sender, receiver, amount) {
+        this.sender = sender;
+        this.receiver = receiver;
+        this.amount = amount;
+    }
+    toString() {
+        return `${this.sender} -> ${this.receiver}: $${this.amount}`;
+    }
+}
+
+async function sha256(text) {
+    const encoder = new TextEncoder();
+    const data = encoder.encode(text);
+    const hashBuffer = await crypto.subtle.digest('SHA-256', data);
+    const hashArray = Array.from(new Uint8Array(hashBuffer));
+    return hashArray.map(byte => byte.toString(16).padStart(2, '0')).join('');
+}
